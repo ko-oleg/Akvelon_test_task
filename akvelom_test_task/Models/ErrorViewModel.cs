8ac9456089ca0c5a5107de63cbd62c2221d4fb60
Oleg Kogay <olegatorkogay@gmail.com>
2022-12-19 18:01:03 +0600

    start

---
+++ akvelom_test_task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace akvelom_test_task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
